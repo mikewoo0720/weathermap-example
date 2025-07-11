b9b1425581a8cd08c1529310070a8a9980068bc8
mikewoo0720 <dnwhdgus93@gmail.com>
2025-07-11 14:33:16 +0900

    5

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const getCurrentPosition = (lat, lon) => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=99844a65192fc302a653f2c29a368452`;
+  /*'~' -> 파이썬 f스트링 역할*/
+
+  console.log(URL);
+};
+
 const getPosition = (position) => {
-  const { latitude, longitude } = position.coords;/*구조 분해 할당 파이썬 튜플~*/
-  console.log("위도 :", latitude);
-  console.log("경도 :", longitude);
-  
+  const { latitude, longitude } = position.coords;/*구조 분해 할당 파이썬 배울때 튜플에서 했죠~*/
+  getCurrentWeather(latitude, longitude);
 };
 
 const errorHandle = (error) => {
   console.error(error);
 };
 
+/*geolocation없을 경우*/
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(getPosition, errorHandle);
 } else {
